83249f610d4a62d28797ddae1263f4cc65fedeaf
berkinmentas <berkinmentas@gmail.com>
2024-02-24 19:38:57 +0300

    update PointController

--- OSMApp/Controllers/PointController.cs
+++ OSMApp/Controllers/PointController.cs
@@ -18,15 +18,26 @@ namespace OSMApp.Controllers
         {
             try
             {
-                var createPoint = new Point
+                bool nameExists = !string.IsNullOrEmpty(point.PointName) && pointManager.TGetByName(point.PointName) != null;
+                bool numberExists = point.PointName != null && pointManager.TGetByNumber(point.PointNumber) != null;
+                bool coordinatesExists = point.Longitude != null && point.Latitude != null && pointManager.TGetByXY(point.Latitude, point.Longitude) != null;
+                if (nameExists || numberExists || coordinatesExists)
                 {
-                    PointName = point.PointName,
-                    PointNumber = point.PointNumber,
-                    Latitude = point.Latitude,
-                    Longitude = point.Longitude,
-                };
-                pointManager.TAdd(createPoint);
-                return new Response { Data = createPoint, Message = "Point added successfully", Success = true };
+                    return new Response { Data = null, Message = "The provided point coordinates, name or number already exists", Success = false };
+                }
+                else
+                {
+                    var createPoint = new Point
+                    {
+                        PointName = point.PointName,
+                        PointNumber = point.PointNumber,
+                        Latitude = point.Latitude,
+                        Longitude = point.Longitude,
+                    };
+                    pointManager.TAdd(createPoint);
+                    return new Response { Data = createPoint, Message = "Point added successfully", Success = true };
+                }
+                
             }
             catch (Exception ex)
             {
@@ -48,36 +59,73 @@ namespace OSMApp.Controllers
             {
                 var points = pointManager.GetList();
                 return new Response { Data = points, Message = "Get points list successful.", Success = true };
-            }else if (!string.IsNullOrEmpty(PointName)){
+            }else if (!string.IsNullOrEmpty(PointName) && string.IsNullOrEmpty(PointNumber) && !Latitude.HasValue && !Longitude.HasValue)
+            {
                 var  pointGetByName = pointManager.TGetByName(PointName);
-                return new Response { Data= pointGetByName, Message ="Name found", Success = true };
+                if(pointGetByName != null)
+                {
+                    return new Response { Data = pointGetByName, Message = "Name found", Success = true };
+                }
+                return new Response { Data= null, Message ="Name didn'n find.", Success = false };
             }
-            else if (!string.IsNullOrEmpty(PointNumber)){
+            else if (string.IsNullOrEmpty(PointName) && !string.IsNullOrEmpty(PointNumber) && !Latitude.HasValue && !Longitude.HasValue)
+            {
                 var pointGetByNumber = pointManager.TGetByNumber(PointNumber);
-                return new Response { Data = pointGetByNumber, Message = "Number found", Success = true };
+                if (pointGetByNumber != null)
+                {
+                    return new Response { Data = pointGetByNumber, Message = "Number found", Success = true };
+                }
+                return new Response { Data = null, Message = "Number didn't find.", Success = false };
             }
-            else if (Latitude.HasValue && Longitude.HasValue)
+            else if (string.IsNullOrEmpty(PointName) && string.IsNullOrEmpty(PointNumber) && Latitude.HasValue && Longitude.HasValue)
             {
                 var pointGetByLatitude = pointManager.TGetByXY(Latitude, Longitude);
-                return new Response { Data = pointGetByLatitude, Message = "Number found", Success = true };
+                if (pointGetByLatitude != null)
+                {
+                    return new Response { Data = pointGetByLatitude, Message = "Coordinate found", Success = true };
+                }
+                return new Response { Data = null, Message = "Coordinate didn't find.", Success = false };
+
             }
-            else if( string.IsNullOrEmpty(PointNumber) && Latitude.HasValue && Longitude.HasValue)
+            else if(string.IsNullOrEmpty(PointName) && !string.IsNullOrEmpty(PointNumber) && Latitude.HasValue && Longitude.HasValue)
             {
                 var pointGetByNumberAndXY = pointManager.TGetByXYAndNumber(PointNumber, Latitude, Longitude);
-                return new Response { Data = pointGetByNumberAndXY, Message = "Point found", Success = true };
-            }else if(string.IsNullOrEmpty(PointName) && Latitude.HasValue && Longitude.HasValue)
+                if (pointGetByNumberAndXY != null)
+                {
+                    return new Response { Data = pointGetByNumberAndXY, Message = "Coordinate founded by number and x-y", Success = true };
+                }
+                return new Response { Data = null, Message = "Coordinate didn't find.", Success = false };
+            }else if(!string.IsNullOrEmpty(PointName) && string.IsNullOrEmpty(PointNumber) && Latitude.HasValue && Longitude.HasValue)
             {
                 var pointGetByNameAndXY = pointManager.TGetByXYAndName(Latitude, Longitude, PointName);
-                return new Response { Data = pointGetByNameAndXY, Message = "Point found", Success = true };
-            }else if(string.IsNullOrEmpty(PointNumber) && string.IsNullOrEmpty(PointName))
-            {
-                var pointGetByNumberAndName = pointManager.TGetByNumberAndName(PointNumber, PointName);
-                return new Response { Data = pointGetByNumberAndName, Message = "Point found", Success = true };
+                if (pointGetByNameAndXY != null)
+                {
+                    return new Response { Data = pointGetByNameAndXY, Message = "Coordinate founded by name and x-y", Success = true };
+                }
+                return new Response { Data = null, Message = "Coordinate didn't find.", Success = false };
 
-            }else 
+            }
+            else if (!string.IsNullOrEmpty(PointName) && !string.IsNullOrEmpty(PointNumber) && !Latitude.HasValue && !Longitude.HasValue)
+            {
+                var pointGetByNumberName = pointManager.TGetByNumberAndName(PointName, PointNumber);
+                if (pointGetByNumberName != null)
+                {
+                    return new Response { Data = pointGetByNumberName, Message = "Name and Number found", Success = true };
+                }
+                else
+                {
+                    return new Response { Data = null, Message = "Name and Number didn't find", Success = false };
+                }
+
+            }
+            else 
             {
                 var pointGetByNumberAndNameAndXY = pointManager.TGetByNumberAndNameAndXY(PointNumber,PointName, Latitude, Longitude);
-                return new Response { Data = pointGetByNumberAndNameAndXY, Message = "Point found", Success = true };
+                if (pointGetByNumberAndNameAndXY != null)
+                {
+                    return new Response { Data = pointGetByNumberAndNameAndXY, Message = "Coordinate founded by name and number and x-y", Success = true };
+                }
+                return new Response { Data = null, Message = "Coordinate didn't find.", Success = false };
 
             }
             
